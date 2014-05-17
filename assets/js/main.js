cbe196fa4d9ec31dbd6120187a74a65f81402b5f
Chase Hutchins <syntaqx@gmail.com>
2014-05-16 19:23:09 -0600

    adding blog news and drew pictures

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,5 +8,34 @@ $(document).ready(function () {
     $(this).ekkoLightbox()
   })
 
+  $.getFeed({
+    url: 'http://blog.armoractive.com/feed/atom',
+    success: function (feed) {
+      if (!feed || !feed.items) {
+        return
+      }
+
+      $('.blog-posts-loading').hide()
+
+      var items = feed.items.slice(0, 4)
+
+      for (var i = 0; i < 4; i++) {
+
+        if (typeof items[i] !== 'undefined') {
+            var description = $(items[i].description).first().html()
+
+            if (description.length > 255) {
+                description = description.substring(0, 252) + '...'
+            }
+
+            $('.blog-posts-' + i).html([
+                '<h4><a href="' + items[i].link + '">' + items[i].title + '</a></h4>',
+                '<p>' + description + '</p>'
+            ].join('\n'))
+        }
+      }
+    }
+  })
+
 
 })
