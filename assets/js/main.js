db06fdc382c8e6fa8c1bcf41aeb8708bcb8ba649
Chase Hutchins <syntaqx@gmail.com>
2014-05-16 18:31:08 -0600

    removing posts functionality and blog, also adding lightbox support

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
 
   $('[data-toggle="tooltip"]').tooltip()
 
+  // Delegate lightbox functionality
+  $(document).delegate('*[data-toggle="lightbox"]', 'click', function (e) {
+    e.preventDefault()
+    $(this).ekkoLightbox()
+  })
+
+
 })
