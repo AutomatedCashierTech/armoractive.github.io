5ce207f867cd94a68c76f636c1883602b19f9e02
Chase Hutchins <syntaqx@gmail.com>
2014-05-20 13:14:31 -0600

    fixing smooth scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,4 +37,20 @@ $(document).ready(function () {
     }
   })
 
+  // Performs a smooth page scroll to an anchor on the same page.
+  $('a[href*=#]:not([href=#]):not([data-slide]):not([data-target])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash)
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']')
+
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000)
+
+        return false
+      }
+    }
+  })
+
 })
