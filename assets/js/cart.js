c6ed6aa1af288ce3f61a78f06ab35894def31b4b
Chase Hutchins <syntaqx@gmail.com>
2014-06-13 11:35:32 -0600

    using correct quote form

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -38,8 +38,12 @@
       html += '<li class="disabled"><a href="#">No items have been added</a></li>'
     }
 
-    html += '<li class="divider"></li><li><a href="http://www2.armoractive.com/l/28102/2013-11-05/tq6v3?products_selected=' + encodeURIComponent(uri.substring(0, uri.length - 2)) + '">Request Quote</a></li>'
+    html += '<li class="divider"></li><li><a href="http://www2.armoractive.com/l/28102/2014-04-03/y3lyc?products_selected=' + encodeURIComponent(uri.substring(0, uri.length - 2)) + '">Request Quote</a></li>'
     html += '<li class="divider"></li><li><a href="#" class="clear-quote">Clear Quote</a></li>'
+
+    /**
+    <iframe src="http://www2.armoractive.com/l/28102/2014-04-03/y3lyc" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
+    */
 
     $('.shopping-cart .dropdown-menu').html(html)
   }
