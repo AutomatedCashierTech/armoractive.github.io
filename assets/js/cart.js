e12c02780f4d9b39276eec1a8d72c1e0c0f53f35
Chase Hutchins <syntaqx@gmail.com>
2014-06-13 10:47:51 -0600

    adding base cart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,8 +1,22 @@
 (function () {
 
+  var cart = {
+  }
+
   $('[data-click="cart"]').on('click', function (e) {
     e.preventDefault()
-    console.log('Adding to cart')
+
+    var $this   = $(this)
+    var product = $this.data('product')
+
+    if (cart[product]) {
+      cart[product]++
+    } else {
+      cart[product] = 1
+    }
+
+    console.log(cart)
+
     return false
   })
 
