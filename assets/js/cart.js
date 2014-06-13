af91570e1193174546150a7508f9a5526a1bfde0
Chase Hutchins <syntaqx@gmail.com>
2014-06-13 10:52:23 -0600

    adding more baseline functionality

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -3,6 +3,22 @@
   var cart = {
   }
 
+  function render() {
+    var html = ''
+
+    if (cart.length !== 0) {
+      for (var key in cart) {
+        html += '<li class="disabled"><a href="#">' + key + '<span class="badge">' + cart[key] + '</span></a></li>'
+      }
+    } else {
+      html += '<li class="disabled">Nothing here!</li>'
+    }
+
+    html += '<li class="divider"></li><li><a href="#">Request Quote</a></li>'
+
+    $('.shopping-cart .dropdown-menu').html(html)
+  }
+
   $('[data-click="cart"]').on('click', function (e) {
     e.preventDefault()
 
@@ -15,9 +31,11 @@
       cart[product] = 1
     }
 
-    alert(cart.stringify())
+    render()
 
     return false
   })
+
+  render()
 
 })()
