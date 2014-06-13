160fd024f3f11de5f9ed9a6c41cc99f98f1c4a6e
Chase Hutchins <syntaqx@gmail.com>
2014-06-13 11:20:31 -0600

    adding right spacing

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -18,7 +18,7 @@
     var count = 0
 
     for (var key in cart) {
-      html += '<li class="disabled"><a href="#">' + key + ' <span class="badge">' + cart[key] + '</span></a></li>'
+      html += '<li class="disabled"><a href="#">' + key + ' &nbsp;&nbsp; <span class="badge pull-right">' + cart[key] + '</span></a></li>'
       uri += key + ' (' + cart[key] + '), '
       count++
     }
