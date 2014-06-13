c3b16ffeaf59eb2d2d41af9c61dd9659ddc11f6c
Chase Hutchins <syntaqx@gmail.com>
2014-06-13 10:58:58 -0600

    adding basic counter logic and url encoding

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -5,16 +5,18 @@
 
   function render() {
     var html = ''
+    var uri = ''
 
     if (cart.length !== 0) {
       for (var key in cart) {
-        html += '<li class="disabled"><a href="#">' + key + '<span class="badge">' + cart[key] + '</span></a></li>'
+        html += '<li class="disabled"><a href="#">' + key + ' <span class="badge">' + cart[key] + '</span></a></li>'
+        uri += key + ' (' + cart[key] + '), '
       }
     } else {
       html += '<li class="disabled">Nothing here!</li>'
     }
 
-    html += '<li class="divider"></li><li><a href="#">Request Quote</a></li>'
+    html += '<li class="divider"></li><li><a href="http://www2.armoractive.com/l/28102/2013-11-05/tq6v3?products_selected=' + encodeURIComponent(url.substring(0, url.length - 1)) + '">Request Quote</a></li>'
 
     $('.shopping-cart .dropdown-menu').html(html)
   }
