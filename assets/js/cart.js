4139b54d91367bd95138b9d0fe00383350ad2446
Chase Hutchins <syntaqx@gmail.com>
2014-06-13 10:45:38 -0600

    trying to fix add to quote buttons

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,7 +1,9 @@
 (function () {
 
-  $('[data-click="cart"]').on('click', function () {
+  $('[data-click="cart"]').on('click', function (e) {
+    e.preventDefault()
     console.log('Adding to cart')
+    return false
   })
 
 })()
