718e772f656b93c81b68f2eef1c1246d0c6bf647
Chase Hutchins <syntaqx@gmail.com>
2014-06-13 11:24:07 -0600

    adding truncate

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -18,7 +18,7 @@
     var count = 0
 
     for (var key in cart) {
-      html += '<li class="disabled"><a href="#"><span class="badge pull-right">' + cart[key] + '</span> ' + key + ' &nbsp;&nbsp;</a></li>'
+      html += '<li class="disabled"><a href="#"><span class="badge pull-right">' + cart[key] + '</span> <span class="truncate">' + key + '</span> &nbsp;&nbsp;</a></li>'
       uri += key + ' (' + cart[key] + '), '
       count++
     }
